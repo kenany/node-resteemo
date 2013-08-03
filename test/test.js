f22969a84eb22ab5ff7a7db502a109f3e7ee02d3
Kenan Yildirim <kenan@kenany.me>
2013-08-02 23:39:03 -0400

    Reduce test redundancy through shared behaviors
    A lot more redundancy remains to be removed though.

--- test/test.js
+++ test/test.js
@@ -12,6 +12,18 @@ var PLAYER_PATH = '/player/' + TEST_PLATFORM + '/' + TEST_SUMMONER;
 var TEAM_PATH = '/team/' + TEST_PLATFORM;
 var DATA_FOLDER = __dirname + '/data/';
 
+var shouldShare = function(fn, file) {
+  it('should be a function', function() {
+    fn.should.be.a('function');
+  });
+  it('should return an object', function() {
+    this.response.should.be.an('object');
+  });
+  it('should return the correct response', function() {
+    this.response.should.deep.equal(require(file));
+  });
+};
+
 describe('node-resteemo', function() {
   it('should export a function', function() {
     resteemo.should.be.a('function');
@@ -24,37 +36,34 @@ describe('node-resteemo', function() {
   });
 
   describe('player', function() {
-    before(function(done) {
+    beforeEach(function(done) {
+      var that = this;
+
       scout
         .get(PLAYER_PATH)
         .replyWithFile(200, DATA_FOLDER + 'profile.json');
 
       teemo.player(TEST_PLATFORM, TEST_SUMMONER, function(error, profile) {
         if (error) return done(error);
-        this.profile = profile;
+        that.response = profile;
         done();
       });
     });
-    after(function() {
-      this.profile = null;
+    afterEach(function() {
+      this.response = null;
     });
 
-    it('should be a function', function() {
-      teemo.player.should.be.a('function');
-    });
-    it('should return an object that is deep equal', function() {
-      profile.should.be.an('object');
-      profile.should.deep.equal(require(DATA_FOLDER + 'profile.json'));
-    });
+    shouldShare(teemo.player, DATA_FOLDER + 'profile.json');
+
     it('should work with full platform names', function(done) {
       scout
         .get(PLAYER_PATH)
         .replyWithFile(200, DATA_FOLDER + 'profile.json');
 
-      teemo.player(TEST_PLATFORM_FULL, TEST_SUMMONER, function(error, prof) {
+      teemo.player(TEST_PLATFORM_FULL, TEST_SUMMONER, function(error, response) {
         should.not.exist(error);
-        should.exist(prof);
-        prof.should.deep.equal(require(DATA_FOLDER + 'profile.json'));
+        should.exist(response);
+        response.should.deep.equal(require(DATA_FOLDER + 'profile.json'));
         done();
       });
     });
@@ -93,302 +102,254 @@ describe('node-resteemo', function() {
     });
 
     describe('ingame', function() {
-      before(function(done) {
+      beforeEach(function(done) {
+        var that = this;
+
         scout
           .get(PLAYER_PATH + '/ingame')
           .replyWithFile(200, DATA_FOLDER + 'ingame.json');
 
         teemo.player.ingame(TEST_PLATFORM, TEST_SUMMONER, function(error, game) {
           if (error) return done(error);
-          this.game = game;
+          that.response = game;
           done();
         });
       });
-      after(function() {
-        this.game = null;
+      afterEach(function() {
+        this.response = null;
       });
 
-      it('should be a function', function() {
-        teemo.player.ingame.should.be.a('function');
-      });
-      it('should return an object that is deep equal', function() {
-        game.should.be.an('object');
-        game.should.deep.equal(require(DATA_FOLDER + 'ingame.json'));
-      });
+      shouldShare(teemo.player.ingame, DATA_FOLDER + 'ingame.json');
     });
 
     describe('recentGames', function() {
-      before(function(done) {
+      beforeEach(function(done) {
+        var that = this;
+
         scout
           .get(PLAYER_PATH + '/recent_games')
           .replyWithFile(200, DATA_FOLDER + 'recent_games.json');
 
         teemo.player.recentGames(TEST_PLATFORM, TEST_SUMMONER, function(error, games) {
           if (error) return done(error);
-          this.games = games;
+          that.response = games;
           done();
         });
       });
-      after(function() {
-        this.games = null;
+      afterEach(function() {
+        this.response = null;
       });
 
-      it('should be a function', function() {
-        teemo.player.recentGames.should.be.a('function');
-      });
-      it('should return an object that is deep equal', function() {
-        games.should.be.an('object');
-        games.should.deep.equal(require(DATA_FOLDER + 'recent_games.json'));
-      });
+      shouldShare(teemo.player.recentGames, DATA_FOLDER + 'recent_games.json');
     });
 
     describe('influencePoints', function() {
-      before(function(done) {
+      beforeEach(function(done) {
+        var that = this;
+
         scout
           .get(PLAYER_PATH + '/influence_points')
           .replyWithFile(200, DATA_FOLDER + 'influence_points.json');
 
         teemo.player.influencePoints(TEST_PLATFORM, TEST_SUMMONER, function(error, points) {
           if (error) return done(error);
-          this.points = points;
+          that.response = points;
           done();
         });
       });
-      after(function() {
-        this.points = null;
+      afterEach(function() {
+        this.response = null;
       });
 
-      it('should be a function', function() {
-        teemo.player.influencePoints.should.be.a('function');
-      });
-      it('should return an object that is deep equal', function() {
-        points.should.be.an('object');
-        points.should.deep.equal(require(DATA_FOLDER + 'influence_points.json'));
-      });
+      shouldShare(teemo.player.influencePoints, DATA_FOLDER + 'influence_points.json');
     });
 
     describe('runes', function() {
-      before(function(done) {
+      beforeEach(function(done) {
+        var that = this;
+
         scout
           .get(PLAYER_PATH + '/runes')
           .replyWithFile(200, DATA_FOLDER + 'runes.json');
 
         teemo.player.runes(TEST_PLATFORM, TEST_SUMMONER, function(error, runes) {
           if (error) return done(error);
-          this.runes = runes;
+          that.response = runes;
           done();
         });
       });
-      after(function() {
-        this.runes = null;
+      afterEach(function() {
+        this.response = null;
       });
 
-      it('should be a function', function() {
-        teemo.player.runes.should.be.a('function');
-      });
-      it('should return an object that is deep equal', function() {
-        runes.should.be.an('object');
-        runes.should.deep.equal(require(DATA_FOLDER + 'runes.json'));
-      });
+      shouldShare(teemo.player.runes, DATA_FOLDER + 'runes.json');
     });
 
     describe('mastery', function() {
-      before(function(done) {
+      beforeEach(function(done) {
+        var that = this;
+
         scout
           .get(PLAYER_PATH + '/mastery')
           .replyWithFile(200, DATA_FOLDER + 'mastery.json');
 
         teemo.player.mastery(TEST_PLATFORM, TEST_SUMMONER, function(error, pages) {
           if (error) return done(error);
-          this.pages = pages;
+          that.response = pages;
           done();
         });
       });
-      after(function() {
-        this.pages = null;
+      afterEach(function() {
+        this.response = null;
       });
 
-      it('should be a function', function() {
-        teemo.player.mastery.should.be.a('function');
-      });
-      it('should return an object that is deep equal', function() {
-        pages.should.be.an('object');
-        pages.should.deep.equal(require(DATA_FOLDER + 'mastery.json'));
-      });
+      shouldShare(teemo.player.mastery, DATA_FOLDER + 'mastery.json');
     });
 
     describe('leagues', function() {
-      before(function(done) {
+      beforeEach(function(done) {
+        var that = this;
+
         scout
           .get(PLAYER_PATH + '/leagues')
           .replyWithFile(200, DATA_FOLDER + 'leagues.json');
 
         teemo.player.leagues(TEST_PLATFORM, TEST_SUMMONER, function(error, leagues) {
           if (error) return done(error);
-          this.leagues = leagues;
+          that.response = leagues;
           done();
         });
       });
-      after(function() {
-        this.leagues = null;
+      afterEach(function() {
+        this.response = null;
       });
 
-      it('should be a function', function() {
-        teemo.player.leagues.should.be.a('function');
-      });
-      it('should return an object that is deep equal', function() {
-        leagues.should.be.an('object');
-        leagues.should.deep.equal(require(DATA_FOLDER + 'leagues.json'));
-      });
+      shouldShare(teemo.player.leagues, DATA_FOLDER + 'leagues.json');
     });
 
     describe('honor', function() {
-      before(function(done) {
+      beforeEach(function(done) {
+        var that = this;
+
         scout
           .get(PLAYER_PATH + '/honor')
           .replyWithFile(200, DATA_FOLDER + 'honor.json');
 
         teemo.player.honor(TEST_PLATFORM, TEST_SUMMONER, function(error, honor) {
           if (error) return done(error);
-          this.honor = honor;
+          that.response = honor;
           done();
         });
       });
-      after(function() {
-        this.honor = null;
+      afterEach(function() {
+        this.response = null;
       });
 
-      it('should be a function', function() {
-        teemo.player.honor.should.be.a('function');
-      });
-      it('should return an object that is deep equal', function() {
-        honor.should.be.an('object');
-        honor.should.deep.equal(require(DATA_FOLDER + 'honor.json'));
-      });
+      shouldShare(teemo.player.honor, DATA_FOLDER + 'honor.json');
     });
 
     describe('rankedStats', function() {
-      before(function(done) {
+      beforeEach(function(done) {
+        var that = this;
+
         scout
           .get(PLAYER_PATH + '/ranked_stats/season/3')
           .replyWithFile(200, DATA_FOLDER + 'ranked_stats.json');
 
         teemo.player.rankedStats(TEST_PLATFORM, TEST_SUMMONER, 3, function(error, stats) {
           if (error) return done(error);
-          this.stats = stats;
+          that.response = stats;
           done();
         });
       });
-      after(function() {
-        this.stats = null;
+      afterEach(function() {
+        this.response = null;
       });
 
-      it('should be a function', function() {
-        teemo.player.rankedStats.should.be.a('function');
-      });
-      it('should return an object that is deep equal', function() {
-        stats.should.be.an('object');
-        stats.should.deep.equal(require(DATA_FOLDER + 'ranked_stats.json'));
-      });
+      shouldShare(teemo.player.rankedStats, DATA_FOLDER + 'ranked_stats.json');
     });
 
     describe('teams', function() {
-      before(function(done) {
+      beforeEach(function(done) {
+        var that = this;
+
         scout
           .get(PLAYER_PATH + '/teams')
           .replyWithFile(200, DATA_FOLDER + 'teams.json');
 
         teemo.player.teams(TEST_PLATFORM, TEST_SUMMONER, function(error, teams) {
           if (error) return done(error);
-          this.teams = teams;
+          that.response = teams;
           done();
         });
       });
-      after(function() {
-        this.teams = null;
+      afterEach(function() {
+        this.response = null;
       });
 
-      it('should be a function', function() {
-        teemo.player.teams.should.be.a('function');
-      });
-      it('should return an object that is deep equal', function() {
-        teams.should.be.an('object');
-        teams.should.deep.equal(require(DATA_FOLDER + 'teams.json'));
-      });
+      shouldShare(teemo.player.teams, DATA_FOLDER + 'teams.json');
     });
   });
 
   describe('team', function() {
-    before(function(done) {
+    beforeEach(function(done) {
+      var that = this;
+
       scout
         .get(TEAM_PATH + '/tag/WHEELZ')
         .replyWithFile(200, DATA_FOLDER + 'tag.json');
 
       teemo.team(TEST_PLATFORM, 'WHEELZ', function(error, matches) {
         if (error) return done(error);
-        this.matches = matches;
+        that.response = matches;
         done();
       });
     });
-    after(function() {
-      this.matches = null;
+    afterEach(function() {
+      this.response = null;
     });
 
-    it('should be a function', function() {
-      teemo.team.should.be.a('function');
-    });
-    it('should return an object that is deep equal', function() {
-      matches.should.be.an('object');
-      matches.should.deep.equal(require(DATA_FOLDER + 'tag.json'));
-    });
+    shouldShare(teemo.team, DATA_FOLDER + 'tag.json');
 
     describe('leagues', function() {
-      before(function(done) {
+      beforeEach(function(done) {
+        var that = this;
+
         scout
           .get(TEAM_PATH + '/guid/TEAM-1e4caa75-aaf0-11e2-8fcc-782bcb4d0bb2/leagues')
           .replyWithFile(200, DATA_FOLDER + 'team_leagues.json');
 
         teemo.team.leagues(TEST_PLATFORM, 'TEAM-1e4caa75-aaf0-11e2-8fcc-782bcb4d0bb2', function(error, leagues) {
           if (error) return done(error);
-          this.leagues = leagues;
+          that.response = leagues;
           done();
         });
       });
-      after(function() {
-        this.leagues = null;
+      afterEach(function() {
+        this.response = null;
       });
 
-      it('should be a function', function() {
-        teemo.team.leagues.should.be.a('function');
-      });
-      it('should return an object', function() {
-        leagues.should.be.an('object');
-        leagues.should.deep.equal(require(DATA_FOLDER + 'team_leagues.json'));
-      });
+      shouldShare(teemo.team.leagues, DATA_FOLDER + 'team_leagues.json');
     });
   });
   describe('freeWeek', function() {
-    before(function(done) {
+    beforeEach(function(done) {
+      var that = this;
+
       scout
         .get('/service-state/na/free-week')
         .replyWithFile(200, DATA_FOLDER + 'free_week.json');
 
       teemo.freeWeek('na', function(error, week) {
         if (error) return done(error);
-        this.week = week;
+        that.response = week;
         done();
       });
     });
-    after(function() {
-      this.week = null;
+    afterEach(function() {
+      this.response = null;
     });
 
-    it('should be a function', function() {
-      teemo.freeWeek.should.be.a('function');
-    });
-    it('should return an object that is deep equal', function() {
-      week.should.be.an('object');
-      week.should.deep.equal(require(DATA_FOLDER + 'free_week.json'));
-    });
+    shouldShare(teemo.freeWeek, DATA_FOLDER + 'free_week.json');
   });
 });
